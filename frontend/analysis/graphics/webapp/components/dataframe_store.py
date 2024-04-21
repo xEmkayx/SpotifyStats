6f41636ff7c85516b4362fa8cdc0379f648d2c24
Markus <example@example.com>
2024-04-21 15:35:19 +0200

    changes from pull request

--- frontend/analysis/graphics/webapp/components/dataframe_store.py
+++ frontend/analysis/graphics/webapp/components/dataframe_store.py
@@ -29,25 +29,6 @@ class DataframeStore:
         return cls()._instance.store
 
 
-# class DataframeStore:
-#     def __init__(self):
-#         self.store_id = DATAFRAME_STORE_ID
-#         self.store_item = init_store(self.store_id)
-#
-#     def render(self):
-#         return html.Div(
-#             self.store_item
-#         )
-#
-#
-# def init_store(item_id):
-#     store = dcc.Store(
-#         id=item_id,
-#         data=get_default_df(),
-#     )
-#     return store
-
-
 def get_default_df():
     df = pd.read_csv(fr'{df_common_path}\{fn_df_allrounder}.csv', index_col=[0])
     return df.to_dict('records')
