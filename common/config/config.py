868e653bd49a249ccbdfc278e7338a93689a20c6
Markus <example@example.com>
2024-05-25 08:59:14 +0200

    added scope, fixed env variable

--- common/config/config.py
+++ common/config/config.py
@@ -8,9 +8,9 @@ CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
 REDIRECT_URI = os.getenv('SPOTIFY_REDIRECT_URI')
 # todo: check scopes
 SCOPE = 'user-read-recently-played user-library-read user-read-private playlist-read-private ' \
-        'playlist-read-collaborative user-top-read user-read-currently-playing'
+        'playlist-read-collaborative user-top-read user-read-currently-playing playlist-modify-public playlist-modify-private'
 TOKEN_CACHE_FILE_PATH = os.getenv('TOKEN_CACHE_FILE_PATH', default='/app/data/spotify_token_cache')
-SPOTIFY_USERNAME = os.getenv('USERNAME')
+SPOTIFY_USERNAME = os.getenv('SPOTIFY_USERNAME')
 
 # mysql
 # MYSQL_ROOT_PASSWORD = os.getenv('MYSQL_ROOT_PASSWORD')
